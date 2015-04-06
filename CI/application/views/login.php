c9613f168ca401bdf1252451059ae167c66e9bd0
IGenerate <brent.stappaerts@gmail.com>
2015-04-06 13:27:24 +0200

    Login.php in view toegevoegd (Faceboo Login)

---
+++ CI/application/views/login.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+<title>Facebook login</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+<div class="container">
+    <form>
+
+        <?php if (@$user_profile): ?>
+        <h2><?=$user_profile['name']?></h2>
+        <a href="<?=$user_profile['link']?>">View Profile</a>
+        <a href="<?= $logout_url ?>">Logout</a> 
+        <?php else: ?>
+        <h2>Login met Facebook</h2>
+        <a href="<?= $login_url ?>">Login</a> 
+        <?php endif; ?>
+    </form>
+</div>
+
+</body>
+
+</html>
