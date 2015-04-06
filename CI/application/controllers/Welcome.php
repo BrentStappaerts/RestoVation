cc079b9c7092156d733780c6e2215a6db1bd7472
IGenerate <brent.stappaerts@gmail.com>
2015-04-06 13:22:35 +0200

    Welcome.php in controllers aangepast voor Facebook Login

--- CI/application/controllers/Welcome.php
+++ CI/application/controllers/Welcome.php
@@ -1,25 +1,62 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+ public function __construct(){
+  parent::__construct();
+
+        // To use site_url and redirect on this controller.
+        $this->load->helper('url');
+ }
+
+ public function login(){
+
+  $this->load->library('facebook'); // Automatically picks appId and secret from config
+        // OR
+        // You can pass different one like this
+        //$this->load->library('facebook', array(
+        //    'appId' => 'APP_ID',
+        //    'secret' => 'SECRET',
+        //    ));
+
+  $user = $this->facebook->getUser();
+        
+        if ($user) {
+            try {
+                $data['user_profile'] = $this->facebook->api('/me');
+            } catch (FacebookApiException $e) {
+                $user = null;
+            }
+        }else {
+            $this->facebook->destroySession();
+        }
+
+        if ($user) {
+
+            $data['logout_url'] = site_url('welcome/logout'); // Logs off application
+            // OR 
+            // Logs off FB!
+            // $data['logout_url'] = $this->facebook->getLogoutUrl();
+
+        } else {
+            $data['login_url'] = $this->facebook->getLoginUrl(array(
+                'redirect_uri' => site_url('welcome/login'), 
+                'scope' => array("email") // permissions here
+            ));
+        }
+        $this->load->view('login',$data);
+
+ }
+
+    public function logout(){
+
+        $this->load->library('facebook');
+
+        // Logs off session from website
+        $this->facebook->destroySession();
+        // Make sure you destory website session as well.
+
+        redirect('welcome/login');
+    }
+
 }
