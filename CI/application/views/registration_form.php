6ab127b06689cd0b6a0e9db6cc7d3033f348fb0b
Tijs <tijsvervoort@gmail.com>
2015-05-05 13:06:51 +0200

    als het nu ni lukt word ik zot

--- CI/application/views/registration_form.php
+++ CI/application/views/registration_form.php
@@ -14,12 +14,25 @@ echo "<div class='error_msg'>";
 echo validation_errors();
 echo "</div>";
 echo form_open('user_authentication/new_user_registration');
-echo form_label('Name : ');
-echo form_input('name');
-echo form_label('Surname : ');
-echo form_input('surname');
-echo form_label('Create Username : ');
-echo form_input('username');
+echo form_label('Naam : ');
+echo form_input('naam');
+echo form_label('Voornaam : ');
+echo form_input('voornaam');
+echo form_label('Kies restaurantnaam : ');
+echo form_input('gebruikersnaam');
+
+echo form_label('Straat: ');
+echo form_input('straat');
+
+echo form_label('Huisnummer : ');
+echo form_input('huisnummer');
+
+echo form_label('Gemeente : ');
+echo form_input('gemeente');
+
+echo form_label('Telefoonnummer: ');
+echo form_input('telefoonnummer');
+
 echo "<div class='error_msg'>";
 if (isset($message_display)) {
 echo $message_display;
@@ -31,8 +44,8 @@ $data = array(
 'name' => 'email_value'
 );
 echo form_input($data);
-echo form_label('Password : ');
-echo form_password('password');
+echo form_label('Passwoord : ');
+echo form_password('passwoord');
 echo form_submit('submit', 'Sign Up');
 echo form_close();
 ?>
