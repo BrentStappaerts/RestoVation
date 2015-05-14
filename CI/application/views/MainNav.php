88faf11839ea4f414ffdc6999e9c0265cf2c25ed
Tijs <tijsvervoort@gmail.com>
2015-05-14 15:31:19 +0200

    links in navigatiebar werken nu, vul aan naar believen

--- CI/application/views/MainNav.php
+++ CI/application/views/MainNav.php
@@ -1,10 +1,17 @@
+<!--Pas deze pagina aan wanneer je wil, als er extra links nodig zijn. 
+Gewoon binnen de site_url() verwijzen naar "controller/functie"-->
+
+<?php  
+$this->load->helper('url');
+?>
 <div class="row">
     <div class="span12">
      <ul class="nav nav-tabs"> 
 
-            <li> <a href="#">Home</a></li>
-            <li> <a href="fb_login/login">Klant login</a></li>
-            <li> <a href="user_authentication/user_login_show">Restaurant login</a></li>
-    </ul>
-    </div>
+       
+          	<li> <a href="<?php echo site_url("fb_login/login");?>">home</a></li>
+            <li> <a href="<?php echo site_url("fb_login/login");?>">Klant login</a></li>
+            <li> <a href="<?php echo site_url("user_authentication/user_login_show");?>">Restaurant login</a></li>
+
+ </div>
     </div>
