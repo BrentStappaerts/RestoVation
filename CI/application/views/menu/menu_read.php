2513d8b3a0a80049bd390a309d88fec4b133d8ee
Tom Vanderheyden <tom.vanderheyden01@gmail.com>
2015-05-15 21:30:05 +0200

    Add_Dish hernoemd naar index() = logischer ipv all records bij controller "menu" -Knop voor return naar all dishes -Knop voor Add dish

--- CI/application/views/menu/menu_read.php
+++ CI/application/views/menu/menu_read.php
@@ -40,6 +40,8 @@
         </tr>
     <?php endforeach;?>
     </table>
+    
+    <input type="button" name="btnAdd" id="btnAdd" value="Gerecht toevoegen" onclick="window.location.href='<?php echo base_url() ?>index.php/menu/adddish'"/>
  
 </body>
 </html>
