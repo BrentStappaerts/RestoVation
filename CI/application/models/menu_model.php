9db6b34d19c658835b42ba7fa61695c45e00b608
Robin Willekens <willekensrobin@hotmail.com>
2015-05-18 18:34:12 +0200

    Menu aangepast zoals Tables en onnodige bestanden verwijderd

--- CI/application/models/menu_model.php
+++ CI/application/models/menu_model.php
@@ -1,26 +1,48 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class menu_model extends CI_Model{
-function __construct() {
-parent::__construct();
-    
+class menu_model extends CI_Model {
+
+	public function __construct()
+	{
+		$this->load->database();
+        $this->load->library('session');
+        $this->load->helper('url');
+	}
+
+	function getAll() {
+		$this->db->select('gerechtid, gerechtnaam, gerechttype, gerechtprijs');
+		$query = $this->db->get('tbl_menu');
+
+		return $query->result_array();
+	}
+	
+	function getMenu($id) {
+		$this->db->select('gerechtid, gerechtnaam, gerechttype, gerechtprijs');
+		$this->db->where('gerechtid', $id);
+		$query = $this->db->get('tbl_menu');
+
+		return $query->row_array();
+	}
+
+	
+	public function insertDish($data)
+	{
+		$this->db->insert('tbl_menu', $data);
+		
+		$id = $this->db->insert_id();
+		
+		return (isset($id)) ? $id : FALSE;
+	}
+
+	public function updateDish($id, $data)
+	{
+		$this->db->where('gerechtid', $id);
+		$this->db->update('tbl_menu', $data);
+	}
+	
+	public function delDish($id)
+	{
+		$this->db->where('gerechtid', $id);
+		$this->db->delete('tbl_menu');
+	}
 }
- 
-function formInsert($data){
-    // Inserting in Table(menu) of Database(restovation)
-    $this->db->insert('tbl_menu', $data);
-}    
-    
-function getAllRecords()
-{
-    $this->load->database();
-    $q = $this->db->get("tbl_menu");
-    if($q->num_rows() > 0)
-    {
-        return $q->result();
-    }
-    return array();
-}
- 
-}
-?>
